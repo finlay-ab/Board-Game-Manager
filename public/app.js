68424c99cb3f16c2dc026fe4e13c7a95f70fabb0
finlay brydges <100164471+finlay-ab@users.noreply.github.com>
2024-09-06 21:12:51 +0100

    testing input and output to firebase database

--- public/app.js
+++ public/app.js
@@ -1,6 +1,15 @@
 document.addEventListener("DOMContentLoaded", event =>{
     const app = firebase.app;
 
+    const db = firebase.firestore();
+    const myPost = db.collection('posts').doc('firstpost');
+
+    myPost.onSnapshot(doc => {
+        const data = doc.data();
+        document.querySelector('#title').innerHTML = data.title;
+    })
+
+
 });
 
 function googleLogin() {
@@ -14,4 +23,10 @@ function googleLogin() {
             console.log(user)
         })
         .catch(console.log);
+}
+
+function updatePost(e) {
+    const db = firebase.firestore();
+    const myPost = db.collection('posts').doc('firstpost');
+    myPost.update({title: e.target.value})
 }
